d743662692fd857142cbf382eb4f742bc058f9d9
Dillon Winchell <dwinchel@redhat.com>
2025-07-14 12:30:43 -0400

    Added setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo "Script is running from: ${SCRIPT_DIR}"
 
 # Check if we are logged into OpenShift
-oc whoami > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+
+echo "Checking if user is logged into OpenShift ..."
+if ! oc whoami > /dev/null 2>&1 ; then
   echo "Error: please login to Openshift first, using 'oc login' or similar."
   exit 1
 fi
 
 # Check if the project exists
 PROJECT_NAME="ocp-capabilities-demo"
-oc get project "${PROJECT_NAME}" > /dev/null 2>&1
-if [ $? -eq 0 ]; then
+
+echo "Checking if project ${PROJECT_NAME} already exists ..."
+if oc get project "${PROJECT_NAME}" > /dev/null 2>&1 ; then
   echo "Project ${PROJECT_NAME} already exists. Reusing it."
 else
   echo "Creating project ${PROJECT_NAME}."
@@ -19,9 +24,10 @@ else
 fi
 
 echo "Applying manifests ..."
-oc apply -f ./manifests/manifests/bogohttp-deployment.yaml
-oc apply -f ./manifests/manifests/bogohttp-hpa.yaml
-oc apply -f ./manifests/manifests/bogohttp-route.yaml
-oc apply -f ./manifests/manifests/bogohttp-svc.yaml
+MANIFESTS_DIR="${SCRIPT_DIR}/manifests"
+oc apply -f "${MANIFESTS_DIR}/bogohttp-deployment.yaml"
+oc apply -f "${MANIFESTS_DIR}/bogohttp-hpa.yaml"
+oc apply -f "${MANIFESTS_DIR}/bogohttp-route.yaml"
+oc apply -f "${MANIFESTS_DIR}/manifests/bogohttp-svc.yaml"
 
 echo "Done."
